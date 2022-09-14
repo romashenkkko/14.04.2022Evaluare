6d6cb1de86c60a57ef76c5992fe94533af719860
romashenkkko <70690141+romashenkkko@users.noreply.github.com>
2022-09-14 18:37:37 +0300

    Update python2.py

--- python2.py
+++ python2.py
@@ -2,12 +2,13 @@ def EvalElem():
     n = int(input("Introdu numarul de elemente: "))
     list = []
     for i in range(n):
-        elem = input("Introdu elemente")
+        elem = eval(input("Elementul " + str(i) + " este:"))
         if type(elem) is int:
-            list.append(elem)
+            list.append(int(elem))
         else:
-            print("Nu e nm intrg")
+            print("Nu e nm intreg")
     return list
+
 
 lista1 = EvalElem()
 print(lista1)
